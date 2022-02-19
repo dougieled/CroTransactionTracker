15e922d1ca0e04af1d01b5014078f02f3d3908ee
Ryan Dougan <dougieled@hotmail.co.uk>
2022-02-19 21:55:51 +0800

    fix errors

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 const manifestJSON = require('./public/manifest.json')
 
 module.exports = {
-  runtimeCompiler: true,
-  pwa: {
-    themeColor: manifestJSON.theme_color,
-    name: manifestJSON.short_name,
-    msTileColor: manifestJSON.background_color,
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      swSrc: 'service-worker.js'
-    }
-  }
+  runtimeCompiler: true
 }
