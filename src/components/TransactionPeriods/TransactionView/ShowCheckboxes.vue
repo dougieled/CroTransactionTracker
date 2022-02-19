367c76d29a85232d0d95f2da21b805b1e08c66d8
Ryan Dougan <dougieled@hotmail.co.uk>
2022-02-20 00:11:11 +0800

    CHECKBOX icons fix

--- src/components/TransactionPeriods/TransactionView/ShowCheckboxes.vue
+++ src/components/TransactionPeriods/TransactionView/ShowCheckboxes.vue
@@ -5,6 +5,8 @@
         v-model="showTransactions"
         dense
         label="Show Transactions?"
+        :on-icon="'check_box'"
+        :off-icon="'check_box_outline_blank'"
       ></v-checkbox>
     </v-col>
     <v-col class="pb-0 pt-0" cols="4">
@@ -12,6 +14,8 @@
         v-model="showDeposits"
         dense
         label="Show Deposits?"
+        :on-icon="'check_box'"
+        :off-icon="'check_box_outline_blank'"
       ></v-checkbox>
     </v-col>
     <v-col class="pb-0 pt-0" cols="4">
@@ -19,6 +23,8 @@
         v-model="showATMWithdrawals"
         dense
         label="Show ATM Withdrawals?"
+        :on-icon="'check_box'"
+        :off-icon="'check_box_outline_blank'"
       ></v-checkbox>
     </v-col>
   </v-row>
@@ -29,15 +35,16 @@ import { mapFields } from 'vuex-map-fields'
 export default {
   name: 'ShowCheckboxes',
   data() {
-    return {
-    }
+    return {}
   },
 
   computed: {
-    ...mapFields('transaction', ['showTransactions','showDeposits','showATMWithdrawals'])
+    ...mapFields('transaction', [
+      'showTransactions',
+      'showDeposits',
+      'showATMWithdrawals'
+    ])
   },
-  methods: {
-
-  }
+  methods: {}
 }
 </script>
