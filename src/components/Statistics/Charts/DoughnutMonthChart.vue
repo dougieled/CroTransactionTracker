1aa0934cf717d2ba884e3f2c636bbfaa227b6a83
Ryan Dougan <dougieled@hotmail.co.uk>
2022-03-08 13:23:28 +0800

    added total to labels also

--- src/components/Statistics/Charts/DoughnutMonthChart.vue
+++ src/components/Statistics/Charts/DoughnutMonthChart.vue
@@ -24,26 +24,26 @@ export default {
     return {
       options: {
         pieceLabel: {
-        mode: 'percentage',
-        precision: 1
-      },
+          mode: 'percentage',
+          precision: 1
+        },
         responsive: true,
         maintainAspectRatio: false,
         layout: {
           padding: 0
         },
         animation: {
-            animateScale: true,
-            animateRotate: true
+          animateScale: true,
+          animateRotate: true
         }
       }
     }
   },
-  mounted(){
+  mounted() {
     this.renderChart(this.chartData, this.options)
   },
   computed: {
-    ...mapGetters('transaction', ['transactionNoDeposit']),
+    ...mapGetters('transaction', ['transactionNoDeposit', 'currencySymbol']),
     transactionChartData() {
       let timestamps = this.transactionNoDeposit.map(x =>
         x.timestamp.format('MMMM YY')
@@ -53,26 +53,36 @@ export default {
         let filteredTransactions = this.transactionNoDeposit.filter(
           c => c.timestamp.format('MMMM YY') === x
         )
-        return {
-          date: x,
-          transactions: filteredTransactions,
-          amountTotal: parseFloat(filteredTransactions
+        let amountTotalOfMonth = parseFloat(
+          filteredTransactions
             .map(x => x.amountFormatted)
             .reduce((a, b) => {
               return a + b
-            }, 0).toFixed(2)),
-            randomColor:randomColor()
+            }, 0)
+            .toFixed(2)
+        )
+        return {
+          date: x,
+
+          transactions: filteredTransactions,
+          amountTotal: amountTotalOfMonth,
+          label: `${x} (${this.currencySymbol}${amountTotalOfMonth})`,
+          randomColor: randomColor()
         }
       })
-      return data.sort((a,b) => new moment(a.date).format('YYYYMM') - new moment(b.date).format('YYYYMM'))
+      return data.sort(
+        (a, b) =>
+          new moment(a.date).format('YYYYMM') -
+          new moment(b.date).format('YYYYMM')
+      )
     },
     chartData() {
       return {
-        labels: this.transactionChartData.map(x => x.date),
+        labels: this.transactionChartData.map(x => x.label),
         datasets: [
           {
             backgroundColor: this.transactionChartData.map(x => x.randomColor),
-            data: this.transactionChartData.map(x => x.amountTotal),
+            data: this.transactionChartData.map(x => x.amountTotal)
           }
         ]
       }
