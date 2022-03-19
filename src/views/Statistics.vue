5df84a725fa0632ac897b08acab52be224364dc3
Ryan Dougan <dougieled@hotmail.co.uk>
2022-03-19 16:06:38 +0800

    add loading to stats page when no transactions returned yet

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid>
-    <v-row v-if="transactions.length>0">
+    <v-row v-if="transactions.length > 0 && !isLoading">
       <v-col cols="12">
         <v-card
           :to="'/'"
@@ -89,41 +89,49 @@
             v-text="'Most spent Recurring'"
           ></v-card-title>
 
-            <most-spent-recurring-table/>
+          <most-spent-recurring-table />
         </v-card>
       </v-col>
-       <v-col cols="12" class="mt-2 pb-0" sm="12" md="4">
+      <v-col cols="12" class="mt-2 pb-0" sm="12" md="4">
         <v-card dark class="pb-12">
           <v-card-title
             class="text-h7 justify-center"
             v-text="'Weekday Spending Average'"
           ></v-card-title>
-          <weekday-spending-average-table/>
+          <weekday-spending-average-table />
         </v-card>
       </v-col>
       <v-col cols="12" class="mt-2 pb-0" sm="12" md="4">
-         <v-card dark>
+        <v-card dark>
           <v-card-title
             class="text-h7 justify-center"
             v-text="'Monthly Spend'"
           ></v-card-title>
           <doughnut-month-chart class="" />
         </v-card>
-        
       </v-col>
       <v-col cols="12" class="pb-8" sm="12" md="12">
         <bar-daily-chart class="pa-6" />
       </v-col>
       <v-col cols="12" class="pb-8" md="12"> </v-col>
     </v-row>
+    <v-row v-else-if="transactions.length === 0 && isLoading" class="mt-3">
+      <v-col cols="12" class="text-center">
+        <h3 class="mb-3">Loading please wait...</h3>
+        <v-progress-circular
+          :size="70"
+          :width="7"
+          color="teal"
+          indeterminate
+        >
+        </v-progress-circular>
+      </v-col>
+    </v-row>
     <v-row v-else class="mt-3">
-       <v-col cols="12">
-        <v-alert
-                  dense
-                  type="error"
-                  icon="error_outline"
-                  >You have no Transactions!</v-alert
-                >
+      <v-col cols="12">
+        <v-alert dense type="error" icon="error_outline"
+          >You have no Transactions!</v-alert
+        >
       </v-col>
     </v-row>
   </v-container>
@@ -148,13 +156,13 @@ export default {
   },
   watch: {
     $route: {
-    immediate: true,
-    handler(to, from) {
-      if(to.name==='Statistics'){
-      this.doOnPageLoad()
+      immediate: true,
+      handler(to, from) {
+        if (to.name === 'Statistics') {
+          this.doOnPageLoad()
+        }
       }
-    },
-  },
+    }
   },
   methods: {
     getData() {
